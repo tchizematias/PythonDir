188738f0245ce7e0ee74a235b5a1c5421128d66c
tdlmatias <tchize7@gmail.com>
2017-05-23 15:45:41 +0100

    Modify the framework from python to python3 and run testes against it

--- src/annoyingLoop.py
+++ src/annoyingLoop.py
@@ -5,4 +5,3 @@ while name != 'your name':
     print('Please type your name.') 
     name = input()
 print('Thank you!')
-
--- src/yourName2.py
+++ src/yourName2.py
@@ -6,5 +6,5 @@ while True:
    print('Please type your name.')
    name = input()
    if name == 'your name':
-      break
+     break
 print('Thank you!')
